9b76cf131c53f3363d68d05d1dee39c6e25da9c5
tokitsu <toki2222son@yahoo.co.jp>
2021-06-23 13:23:56 +0900

    add create

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::EmployeesController < ApiController
   before_action :set_employee, only: [:show]
 
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    render json: { error: '404 not found' }, status: 404
-  end
+  rescue_from Exception, with: :render_status_500
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_status_404
 
   def index
     employees = Employee.select(:id, :name, :department, :gender)
@@ -14,9 +14,30 @@ class Api::V1::EmployeesController < ApiController
     render json: @employee
   end
 
-  private
+  def create
+    employee = Employee.new(employee_params)
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
+  private
+  
   def set_employee
     @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.fetch(:employee, {}).permit(:name, :department, :gender, :birth, :joined_date, :payment, :note)
+  end
+
+  def render_status_404(exception)
+    render json: { errors: [exception] }, status: 404
+  end
+
+  def render_status_500(exception)
+    render json: { errors: [exception] }, status: 500
   end
 end
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,37 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import EmployeeIndexPage from 'EmployeeIndexPage.vue'
+import EmployeeDetailPage from 'EmployeeDetailPage.vue'
+import EmployeeNewPage from 'EmployeeNewPage.vue'
+
+const router = new VueRouter({
+  routes: [
+    { path: '/',
+      name: 'EmployeeIndexPage',
+      component: EmployeeIndexPage  },
+    { path: '/employees/:id(\\d+)',
+      name: 'EmployeeDetailPage',
+      component: EmployeeDetailPage  },
+    { path: '/employees/new',
+      name: 'EmployeeNewPage',
+      component: EmployeeNewPage  },
+  ]
+})
+Vue.use(VueRouter)
+
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
+  router
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+
 </style>
